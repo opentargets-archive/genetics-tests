d538d1680b2c2246471ab4107c23d3fb5be44a13
elipapa <elipapa@users.noreply.github.com>
2018-08-06 20:53:04 +0000

    testing bq connection, pipfile requirements"

---
+++ bq/testbq.py
@@ -0,0 +1,41 @@
+from google.cloud import bigquery
+
+
+def query_stackoverflow():
+    client = bigquery.Client()
+    query_job = client.query("""
+        SELECT
+          CONCAT(
+            'https://stackoverflow.com/questions/',
+            CAST(id as STRING)) as url,
+          view_count
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE tags like '%google-bigquery%'
+        ORDER BY view_count DESC
+        LIMIT 10""")
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
+
+
+def query_v2g():
+    client = bigquery.Client()
+    query_job = client.query("""
+        select distinct(chr_id) as chr 
+        from `g2v_draft.20180725` 
+        order by chr_id desc
+    """)
+
+    results = query_job.result()  # Waits for job to complete.
+
+    for row in results:
+        print("chr: {}".format(row.chr))
+
+
+if __name__ == '__main__':
+    # query public data
+    query_stackoverflow()
+    # query within project
+    query_v2g()
