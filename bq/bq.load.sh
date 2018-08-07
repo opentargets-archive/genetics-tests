058f8471b567137c1d1502579ce2fa302f6bd071
elipapa <elipapa@users.noreply.github.com>
2018-08-07 16:49:03 +0300

    v2d and d2v2g loading

--- bq/bq.load.sh
+++ bq/bq.load.sh
@@ -1,3 +1,9 @@
-bq --location=EU load --skip_leading_rows=1 -F "\t" --source_format=CSV g2v_draft.`date +%Y%m%d` gs://genetics-portal-data/out/v2g/*.csv ./bq.v2g.schema.json
+bq --location=EU load --skip_leading_rows=1 -F "tab" --source_format=CSV g2v_draft.`date +%Y%m%d` "gs://genetics-portal-data/out/v2g/*.csv" ./bq.v2g.schema.json
 
-bq --location=EU load --skip_leading_rows=1 -F "\t" --allow_jagged_rows --source_format=CSV v2d_draft.`date +%Y%m%d` gs://genetics-portal-data/out/v2d/*.csv ./bq.v2d.schema.json
+bq --location=EU load --skip_leading_rows=1 -F "tab" --source_format=CSV v2d_draft.`date +%Y%m%d` "gs://genetics-portal-data/out/v2d/*.csv" ./bq.v2d.schema.json
+
+bq --location=EU load -F "tab" --source_format=CSV v2g_lut_gene.`date +%Y%m%d` "gs://genetics-portal-data/out/v2g-lut-gene/*.csv" gene,chr,start:integer,end:integer
+
+bq --location=EU load -F "tab" --source_format=CSV v2g_lut_rsid.`date +%Y%m%d` "gs://genetics-portal-data/out/v2g-lut-rsid/*.csv" rsid,chr,position:integer
+
+bq --location=EU load --skip_leading_rows=1 -F "tab" --source_format=CSV d2v2g_draft.`date +%Y%m%d` "gs://genetics-portal-data/out/d2v2g/*.csv" ./bq.d2v2g.schema.json
